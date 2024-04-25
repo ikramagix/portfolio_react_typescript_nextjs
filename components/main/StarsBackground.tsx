0a5868c05a49d0d884cd28995d3f756d4094ba79
ikramiste <ikrame.saadi@gmail.com>
2024-04-25 23:39:15 +0300

    corrected embarassing typo - maath restored

--- components/main/StarsBackground.tsx
+++ components/main/StarsBackground.tsx
@@ -4,7 +4,7 @@ import React, { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
 // @ts-ignore
-import * as random from "math/random/dist/maath-random.esm";
+import * as random from "maath/random/dist/maath-random.esm";
 
 const StarBackground = (props: any) => {
   const ref: any = useRef();
